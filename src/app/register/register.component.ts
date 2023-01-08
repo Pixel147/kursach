a5696fe9e21e22b95e580eff499dc5dc2c64d405
Reaper <reaper144@ukr.net>
2023-01-08 18:03:55 +0200

    console.log for register1123123

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  checkLog = true
+  checkPass = true
+  nickname = '';
+  email = '';
+  pass = '';
+  repPass = '';
+
+
+  onNickname(event:any){
+
+    this.nickname = event.target.value
+    if (this.nickname.length < 4)
+    {
+      this.checkLog = false
+    }
+
+    else
+    {
+      this.checkLog = true
+    }
+
+  }
+
+  onEmail(event:any)
+  {
+    this.email = event.target.value
+  }
+
+  onPass(event:any){
+    this.pass = event.target.value
+  }
+
+  onRepPass(event:any)
+  {
+    this.repPass = event.target.value
+  }
+
+  onClick()
+  {
+    console.log(this.nickname)
+    console.log(this.email)
+    console.log(this.pass)
+    console.log(this.repPass)
+  }
+
 
 }
