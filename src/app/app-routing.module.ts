74e66bef5f21208a7eeb8d0ab77a3f2330cd7a8d
Serg <smazich147@gmail.com>
2023-01-08 15:47:16 +0200

    add home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
-  {}
-];
-
+  {path: '', component:HomeComponent},
+  {path: 'login', component: LoginComponent}
+]
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from "./home/home.component";
 import {RouterOutlet} from "@angular/router";
-import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -13,11 +14,12 @@ import { HomeComponent } from './home/home.component';
     LoginComponent,
     HomeComponent
   ],
-    imports: [
-        BrowserModule,
-        NgbModule,
-        RouterOutlet
-    ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    RouterOutlet,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
