ffa885d7e99fc13066e6041e6e8a233442201d94
Reaper <reaper144@ukr.net>
2023-02-15 18:49:02 +0200

    i make a slider for each day in week but i dont have time fields in mySql so Seroja will fix it and test it

--- angular/src/app/owner-worker-panel/owner-worker-panel.component.ts
+++ angular/src/app/owner-worker-panel/owner-worker-panel.component.ts
@@ -11,8 +11,27 @@ import { Options } from '@angular-slider/ngx-slider';
 
 export class OwnerWorkerPanelComponent {
 
-  minValue: number = 0.0;
-  maxValue: number = 23;
+  minValueMon: number = 0;
+  maxValueMon: number = 23;
+  minValueTue: number = 0;
+  maxValueTue: number = 23;
+  minValueWed: number = 0;
+  maxValueWed: number = 23;
+  minValueThu: number = 0;
+  maxValueThu: number = 23;
+  minValueFri: number = 0;
+  maxValueFri: number = 23;
+  minValueSat: number = 0;
+  maxValueSat: number = 23;
+  minValueSun: number = 0;
+  maxValueSun: number = 23;
+  monFlag = true;
+  tueFlag = false;
+  wedFlag = false;
+  thuFlag = false
+  friFlag = false;
+  satFlag = false;
+  sunFlag = false;
   options: Options = {
     floor: 0,
     ceil: 23,
@@ -84,6 +103,48 @@ export class OwnerWorkerPanelComponent {
         console.log(err);
       }
     });
+  }
+
+  MonFlag()
+  {
+    this.monFlag = true;
+    this.tueFlag = this.wedFlag = this.thuFlag = this.friFlag = this.satFlag = this.sunFlag = false;
+  }
+
+  TueFlag()
+  {
+    this.tueFlag = true;
+    this.monFlag = this.wedFlag = this.thuFlag = this.friFlag = this.satFlag = this.sunFlag = false;
+  }
+
+  WedFlag()
+  {
+    this.wedFlag = true;
+    this.monFlag = this.tueFlag = this.thuFlag = this.friFlag = this.satFlag = this.sunFlag = false;
+  }
+
+  ThuFlag()
+  {
+    this.thuFlag = true;
+    this.monFlag = this.wedFlag = this.tueFlag = this.friFlag = this.satFlag = this.sunFlag = false;
+  }
+
+  FriFlag()
+  {
+    this.friFlag = true;
+    this.monFlag = this.wedFlag = this.thuFlag = this.tueFlag = this.satFlag = this.sunFlag = false;
+  }
+
+  SatFlag()
+  {
+    this.satFlag = true;
+    this.monFlag = this.wedFlag = this.thuFlag = this.friFlag = this.tueFlag = this.sunFlag = false;
+  }
+
+  SunFlag()
+  {
+    this.sunFlag = true;
+    this.monFlag = this.wedFlag = this.thuFlag = this.friFlag = this.satFlag = this.tueFlag = false;
   }
 }
 
