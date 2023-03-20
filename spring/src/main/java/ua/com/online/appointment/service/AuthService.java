63c2b78a092676c723bd11c0cfee77f4bf39d4ad
Seroja <87807394+Pixel147@users.noreply.github.com>
2023-03-20 20:58:40 +0200

    Update AuthService.java

--- spring/src/main/java/ua/com/online/appointment/service/AuthService.java
+++ spring/src/main/java/ua/com/online/appointment/service/AuthService.java
@@ -124,7 +124,6 @@ public class AuthService {
             worker.setFridayEnd(LocalTime.of(workerRegistrationRequest.getFridayEnd(),0));
 
             worker.setSaturdayStart(LocalTime.of(workerRegistrationRequest.getSaturdayStart(),0));
-            System.out.println(worker.getSaturdayStart());
             worker.setSaturdayEnd(LocalTime.of(workerRegistrationRequest.getSaturdayEnd(),0));
 
             worker.setSundayStart(LocalTime.of(workerRegistrationRequest.getSundayStart(),0));
